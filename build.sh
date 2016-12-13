5de72355c77ce16999953b81d59cbb235613a53c
Adam Kubica <caffecoder@kaizen-step.com>
2016-12-13 13:01:45 +0100

    Improved rbenv build method

--- build.sh
+++ build.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/env bash
 
-for item in `ls -d cde-*`; do
-    docker build --rm -t "caffecoder/${item}" $item
-done
+DANGLING=$(docker images -f "dangling=true" -q)
+
+if [ ! -z $DANGLING ]; then
+    docker rmi $DANGLING
+fi
+
+docker pull ubuntu:16.04
+
+docker build --rm -t caffecoder/cde-baseimage cde-baseimage
+
+docker build --rm -t caffecoder/cde-storage cde-storage
+
+docker build --rm -t caffecoder/cde-java cde-java
+
+docker build --build-arg RUBY_VERSION=2.3.3 --rm -t caffecoder/cde-rbenv:2.3 cde-rbenv
+docker build --build-arg RUBY_VERSION=2.3.3 --rm -t caffecoder/cde-rbenv:latest cde-rbenv
+
+docker build --rm -t caffecoder/cde-syslog cde-syslog
