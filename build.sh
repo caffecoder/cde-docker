a671f5dc42bcb4c6d9b2aea1a95e9d25e93dcda8
Adam Kubica <xcdr@kaizen-step.com>
2017-05-15 20:16:41 +0200

    Rename caffecoder -> xcdr, ruby version upgrade

--- build.sh
+++ build.sh
@@ -8,13 +8,11 @@ fi
 
 docker pull ubuntu:16.04
 
-docker build --rm -t caffecoder/cde-baseimage cde-baseimage
+docker build --rm -t xcdr/cde-baseimage cde-baseimage
 
-docker build --rm -t caffecoder/cde-storage cde-storage
+docker build --rm -t xcdr/cde-java cde-java
 
-docker build --rm -t caffecoder/cde-java cde-java
+docker build --build-arg RUBY_VERSION=2.3.4 --rm -t xcdr/cde-rbenv:2.3 cde-rbenv
+docker build --build-arg RUBY_VERSION=2.3.4 --rm -t xcdr/cde-rbenv:latest cde-rbenv
 
-docker build --build-arg RUBY_VERSION=2.3.3 --rm -t caffecoder/cde-rbenv:2.3 cde-rbenv
-docker build --build-arg RUBY_VERSION=2.3.3 --rm -t caffecoder/cde-rbenv:latest cde-rbenv
-
-docker build --rm -t caffecoder/cde-syslog cde-syslog
+docker build --rm -t xcdr/cde-syslog cde-syslog
