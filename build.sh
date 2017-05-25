abd341d0fc4da23d81773c8c7c79ddc32f07d3a2
Adam Kubica <xcdr@kaizen-step.com>
2017-05-25 15:03:47 +0200

    Added squashing and purging images available in Docker 1.13

--- build.sh
+++ build.sh
@@ -1,18 +1,22 @@
 #!/usr/bin/env bash
 
-DANGLING=$(docker images -f "dangling=true" -q)
-
-if [ ! -z $DANGLING ]; then
-    docker rmi $DANGLING
-fi
+#DANGLING=$(docker images -f "dangling=true" -q)
+#
+#if [ ! -z $DANGLING ]; then
+#    docker rmi $DANGLING
+#fi
 
 docker pull ubuntu:16.04
 
-docker build --rm -t xcdr/cde-baseimage cde-baseimage
+docker image prune -f
 
-docker build --rm -t xcdr/cde-java cde-java
+docker build --squash --rm -t xcdr/cde-baseimage cde-baseimage
 
-docker build --build-arg RUBY_VERSION=2.3.4 --rm -t xcdr/cde-rbenv:2.3 cde-rbenv
-docker build --build-arg RUBY_VERSION=2.3.4 --rm -t xcdr/cde-rbenv:latest cde-rbenv
+docker build --squash --rm -t xcdr/cde-java cde-java
 
-docker build --rm -t xcdr/cde-syslog cde-syslog
+docker build --squash --build-arg RUBY_VERSION=2.3.4 --rm -t xcdr/cde-rbenv:2.3 cde-rbenv
+docker build --squash --build-arg RUBY_VERSION=2.3.4 --rm -t xcdr/cde-rbenv:latest cde-rbenv
+
+docker build --squash --rm -t xcdr/cde-syslog cde-syslog
+
+docker image prune -f
