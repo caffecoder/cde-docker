54d93e3461eef0a2a56c7bc0afd9cb88b93bdf28
Adam Kubica <xcdr@kaizen-step.com>
2017-09-27 21:25:12 +0200

    Removing experimental --squash

--- build.sh
+++ build.sh
@@ -10,13 +10,13 @@ docker pull ubuntu:16.04
 
 docker image prune -f
 
-docker build --squash --rm -t xcdr/cde-baseimage cde-baseimage
+docker build --rm -t xcdr/cde-baseimage cde-baseimage
 
-docker build --squash --rm -t xcdr/cde-java cde-java
+docker build --rm -t xcdr/cde-java cde-java
 
-docker build --squash --build-arg RUBY_VERSION=2.3.4 --rm -t xcdr/cde-rbenv:2.3 cde-rbenv
-docker build --squash --build-arg RUBY_VERSION=2.3.4 --rm -t xcdr/cde-rbenv:latest cde-rbenv
+docker build --build-arg RUBY_VERSION=2.4.2 --rm -t xcdr/cde-rbenv:2.4 cde-rbenv
+docker build --build-arg RUBY_VERSION=2.4.2 --rm -t xcdr/cde-rbenv:latest cde-rbenv
 
-docker build --squash --rm -t xcdr/cde-syslog cde-syslog
+docker build --rm -t xcdr/cde-syslog cde-syslog
 
 docker image prune -f
