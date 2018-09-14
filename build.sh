66dd09016d4146d7e8f364a09bc3057a3f2e8e41
Adam Kubica <xcdr@kaizen-step.com>
2018-09-14 09:57:33 +0200

    Bug fixes

--- build.sh
+++ build.sh
@@ -5,16 +5,17 @@ docker pull php:7.2-apache
 
 docker system prune -f
 
-docker build --rm -t xcdr/cde-baseimage cde-baseimage
+docker build --rm -t xcdr/cde-baseimage cde-baseimage && docker push xcdr/cde-baseimage
 
-docker build --rm -t xcdr/cde-java cde-java
+docker build --rm -t xcdr/cde-java cde-java && docker push xcdr/cde-java
 
-docker build --build-arg RUBY_VERSION=2.3.7 --rm -t xcdr/cde-rbenv:2.3 cde-rbenv
-docker build --build-arg RUBY_VERSION=2.4.4 --rm -t xcdr/cde-rbenv:2.4 cde-rbenv
-docker build --build-arg RUBY_VERSION=2.4.4 --rm -t xcdr/cde-rbenv:latest cde-rbenv
+docker build --build-arg RUBY_VERSION=2.3.7 --build-arg LIBSSL_PACKAGE=libssl1.0-dev --rm -t xcdr/cde-rbenv:2.3 cde-rbenv && docker push xcdr/cde-rbenv:2.3
+docker build --build-arg RUBY_VERSION=2.4.4 --rm -t xcdr/cde-rbenv:2.4 cde-rbenv && docker push xcdr/cde-rbenv:2.4
+docker build --build-arg RUBY_VERSION=2.4.4 --rm -t xcdr/cde-rbenv:latest cde-rbenv && docker push xcdr/cde-rbenv:latest
 
-docker build --rm -t xcdr/cde-syslog cde-syslog
+docker build --rm -t xcdr/cde-syslog cde-syslog && docker push xcdr/cde-syslog
 
-docker build --rm -t xcdr/cde-roundcube:1.3 cde-roundcube
+docker build --rm -t xcdr/cde-roundcube:1.3 cde-roundcube && docker push xcdr/cde-roundcube
 
 docker system prune -f
+
